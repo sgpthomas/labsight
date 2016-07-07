16dcbe1764f3bf3a451418c9d6eab2ea3bda7adc
Sam Thomas <sgpthomas@gmail.com>
2016-07-07 15:46:30 -0700

    syncing things up

---
+++ lib/labsight/Controller.py
@@ -0,0 +1,8 @@
+import serial
+import serial.tools.list_ports as list
+from serial.serialutil import SerialException
+import io
+from time import sleep
+
+def getMotors (config_folder = ""):
+    # some stuff
