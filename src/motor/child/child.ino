12027c7d4aff8e7a284d4d23d5c897f6f8f2ab36
sbuckleybonanno <sbuckleybonanno@gmail.com>
2016-07-12 15:43:22 -0700

    Reworked the architecture of child.ino. It compiles with a ton of scoping errors because I'm not importing libraries correctly, but hopefully I can fix that later today. Also, implementing structs was unsuccessful, so everything's just hardcoded right now. I might try to implement them later, though I don't have much faith in the Arduino handling objects like that.

--- src/motor/child/child.ino
+++ src/motor/child/child.ino
@@ -1,4 +1,5 @@
 #include <EEPROM.h>
+#include <AFMotor.h>
 
 // identity
 String id = "";
@@ -19,89 +20,23 @@ int encoderB = 4;
 // encoder position
 volatile unsigned int encoderPos = 0;
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  Serial.begin(9600);
-  pinMode(LED, OUTPUT);
+// I tried to implement these structs like the classes, in protocol.py, but they didn't work, so I ended up hardcoding everything. Feel free to try t make them work if you'd like
 
-  // setup encoder pins and turn on internal pull-up resistor
-  pinMode(encoderA, INPUT);
-  digitalWrite(encoderA, HIGH);
+struct Symbol {
+  String GET = "?";
+  String SET = "!";
+  String ANSWER = "$";
+  String OPEN_STREAM = ">";
+  String CLOSE_STREAM = "/";
+};
 
-  pinMode(encoderB, INPUT);
-  digitalWrite(encoderB, HIGH);
-
-  // attach interrupt to keep track of posittion
-  attachInterrupt(digitalPinToInterrupt(encoderA), updateEncoderPos, CHANGE);
-  
-  id = rememberID();
-}
-
-void updateEncoderPos() {
-  
-}
-
-void serialEvent() {
-  if (Serial.available() && !stream) {
-    String symbol = Serial.readStringUntil(' ');
-    String command = Serial.readStringUntil(' ');
-    String info = Serial.readStringUntil('\n');
-    receivedMessage(symbol, command, info);
-    // digitalWrite(LED, HIGH);
-    // delay(500);
-    // digitalWrite(LED, LOW);
-  }
-}
-
-void receivedMessage(String symbol, String command, String info) {  
-//  if (symbol == "?") {
-//    if (command == "listen") {
-//      Serial.println(estResponse);
-//      
-//    } 
-//    
-//    else if (command == "id") {
-//      Serial.println("$ id " + id);
-//    }
-//  }
-//  
-//  if (symbol == "!") {
-//    if (command == "id") {
-//      id = info;
-//      setID(info);
-//      Serial.println("# id " + info);
-//    }
-//  }
-
-  if (command == "version") {
-    Serial.println("$ version " + version_number);
-  }
-
-  else if (command == "id") {
-    if (symbol == "?") { // asking for id
-      Serial.println("$ id " + id);
-    } 
-    
-    else if (symbol == "!") { // set id
-      id = info;
-      setID(info);
-      Serial.println("# id " + info);
-    }
-  }
-
-  else if (command == "step") {
-    if (symbol == "!") { // move and open data stream
-      stream = true;
-      Serial.println("> step " + info);
-      for (int i = 0; i < info.toInt(); i++) {
-        Serial.println(i);
-        // delay(200);
-      }
-      Serial.println("/ step " + info);
-      stream = false;
-    }
-  }
-}
+struct Command {
+  String ID = "id";
+  String STEP = "step";
+  String KILL = "kill";
+  String SPEED = "speed";
+  String VERSION = "version";
+};
 
 String rememberID() {
   char value;
@@ -122,11 +57,89 @@ String rememberID() {
   return id;
 }
 
+
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+
+  // setup encoder pins and turn on internal pull-up resistor
+  pinMode(encoderA, INPUT);
+  digitalWrite(encoderA, HIGH);
+
+  pinMode(encoderB, INPUT);
+  digitalWrite(encoderB, HIGH);
+
+  // attach interrupt to keep track of position
+  attachInterrupt(digitalPinToInterrupt(encoderA), updateEncoderPos, CHANGE);
+  
+  id = rememberID();
+
+  // Set up the motor shield object
+  Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+  // Set up stepper motor object
+  Adafruit_StepperMotor *motor = AFMS.getStepper(Steps_per_revolution, Motor_Shield_Port);
+  // Those 2 arguments need to be set to something
+  // Also, the asterisk means this variable is just a pointer. I don't why it's worth doing that, but it's what the library reference did.
+}
+
+String join(String symbol, String command, String data) {
+  return symbol + " " + command + " " + data;
+}
+
+// ----------------Getter Functions----------------
+
+void getID() {
+  Serial.println(join("$", "id", id));
+}
+
+void getVersion() {
+  Serial.println(join("$", "version", version_number));
+}
+
+// ----------------Setter Functions----------------
+
 void setID(String id) {
   clearMem();
   for (int i = 0; i < id.length(); i++) {
     EEPROM.write(i, id[i]);
   }
+  Serial.println(join("$", "id", id));
+}
+
+void setVersion(String data) { // Does this need to write the version to EEPROM? Why write the ID, and not the version?
+  version_number = data;
+  Serial.println(join("$", "version", version_number));
+}
+
+
+// The below function supports only relative motion
+void setStep(String distance) {
+   stream = true;
+   Serial.println(join(">", "step", distance));
+   if (distance.toInt() > 0) {
+     uint8_t direction = FORWARD;
+   }
+   else if (distance < 0) {
+     uint8_t direction = BACKWARD;
+   }
+   else {
+    return;
+   }
+   for (int i = 0; i < distance.toInt(); i++) {
+     motor.step(1, direction);
+     Serial.println(i);
+     //delay(200);
+   }
+   Serial.println(join(">", "step", distance));
+   stream = false;
+}
+
+void setKill() {
+  motor.release();
+  Serial.println(join("$", "kill", "true"));
 }
 
 void clearMem() {
@@ -135,11 +148,71 @@ void clearMem() {
   }  
 }
 
+void updateEncoderPos() {
+}
+
+void serialEvent() {
+  if (Serial.available() && !stream) {
+    String symbol = Serial.readStringUntil(' ');
+    String command = Serial.readStringUntil(' ');
+    String data = Serial.readStringUntil('\n');
+    receivedMessage(symbol, command, data);
+    // digitalWrite(LED, HIGH);
+    // delay(500);
+    // digitalWrite(LED, LOW);
+  }
+}
+
+void receivedMessage(String symbol, String command, String data) {  
+  if (symbol == "?") {
+    if (command == "id") {
+      getID();
+    }
+    else if (command == "version") {
+      getVersion();
+    }
+//      All of these cases below are probably unnecessary, as the controller can do it for the Arduino:
+//      (Also, I know switch cases won't work here, just imagine they're if statements)
+//      case "step":
+//        getStep();
+//        break;
+//      case "speed":
+//        getSpeed();
+//        break;
+//      case "kill":
+//        getKill(); // I don't know if it's possible to implemetn this function, or worth doing when the controller can do it anyway
+//        break;
+     else {
+        // Send some error message about an incoherent command. This can be defined as a function as well
+    }
+  }
+    
+  
+  else if (symbol == "!") {
+    if (command == "id") {
+      setID(data);
+    }
+    else if (command == "version") {
+      setVersion(data);
+    }
+    else if (command == "step") {
+      setStep(data);
+    }
+    else if (command == "speed") {
+      setSpeed(data); //This function is already in the motor shield library
+    }
+    else if (command == "kill") {
+        setKill();
+    }
+    else {
+      // Send some error message about an incoherent command
+    }
+  }
+}
 int address = 0;
 byte value;
 boolean l = true;
 
-// the loop function runs over and over again forever
 void loop() {
 
 }
